6d1f3742f4605c10734c9d01f480fcb7fe22d628
Alexander Schramm <alex@schram.me>
2021-02-01 12:35:44 +0100

    refa: cluster creation interface

--- packages/@superluminar-io/super-eks/test/constructs/eks-cluster.test.ts
+++ packages/@superluminar-io/super-eks/test/constructs/eks-cluster.test.ts
@@ -1,6 +1,8 @@
 import { expect as expectCDK, haveResource } from "@aws-cdk/assert"
 import * as cdk from "@aws-cdk/core"
-import { EksCluster } from "../../lib/constructs/eks-cluster"
+import * as route53 from "@aws-cdk/aws-route53"
+
+import { SuperEks } from "../../lib/constructs/super-eks"
 
 test("Empty Cluster", () => {
   const app = new cdk.App()
@@ -8,7 +10,9 @@ test("Empty Cluster", () => {
     env: { region: "eu-central-1", account: "1234567891011" },
   })
   // WHEN
-  new EksCluster(stack, "TestCluster", { vpcId: "test" })
+  new SuperEks(stack, "TestCluster", {
+    hostedZone: route53.HostedZone.fromHostedZoneId(stack, "HostedZone", "123"),
+  })
   // THEN
   expectCDK(stack).to(haveResource("Custom::AWSCDK-EKS-Cluster"))
 })
