4971030f60b00b4e1e5dd55eba69857410df52b0
Jan Brauer <jan.brauer@superluminar.io>
2021-02-01 15:45:43 +0100

    Add AWS for Fluent Bit (#11)

---
+++ packages/@superluminar-io/super-eks/test/constructs/fluent-bit.test.ts
@@ -0,0 +1,53 @@
+import "@aws-cdk/assert/jest"
+import { Stack } from "@aws-cdk/core"
+import { Cluster } from "@aws-cdk/aws-eks"
+import * as eks from "@aws-cdk/aws-eks"
+import { FluentBit } from "../../lib/constructs/fluent-bit"
+
+describe("fluent-bit", () => {
+  test("namespace can be configured and created", () => {
+    const stack = new Stack()
+    const cluster = new Cluster(stack, "EKS", {
+      version: eks.KubernetesVersion.V1_18,
+    })
+    new FluentBit(stack, "FluentBit", {
+      cluster,
+      region: "eu-west-1",
+      namespace: "foo",
+      createNamespace: true,
+    })
+    expect(stack).toHaveResource("Custom::AWSCDK-EKS-HelmChart", {
+      Namespace: "foo",
+      CreateNamespace: true,
+    })
+  })
+
+  test('namespace defaults to "kube-system"', () => {
+    const stack = new Stack()
+    const cluster = new Cluster(stack, "EKS", {
+      version: eks.KubernetesVersion.V1_18,
+    })
+    new FluentBit(stack, "FluentBit", {
+      cluster,
+      region: "eu-west-1",
+    })
+    expect(stack).toHaveResource("Custom::AWSCDK-EKS-HelmChart", {
+      Namespace: "kube-system",
+    })
+  })
+
+  test("Cloudwatch is configured correctly", () => {
+    const stack = new Stack()
+    const cluster = new Cluster(stack, "EKS", {
+      version: eks.KubernetesVersion.V1_18,
+    })
+    new FluentBit(stack, "FluentBit", {
+      cluster,
+      region: "eu-west-1",
+    })
+    expect(stack).toHaveResource("Custom::AWSCDK-EKS-HelmChart", {
+      Values:
+        '{"serviceAccount":{"create":false,"name":"fluent-bit"},"firehose":{"enabled":false},"kinesis":{"enabled":false},"elasticsearch":{"enabled":false},"cloudwatch":{"region":"eu-west-1"}}',
+    })
+  })
+})
