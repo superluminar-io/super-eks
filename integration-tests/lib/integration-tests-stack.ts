47070f11e0003c80908a4aded6c87c4fb94300e5
Jan Brauer <jan.brauer@superluminar.io>
2021-02-02 15:33:37 +0100

    Install nginx helm chart

--- integration-tests/lib/integration-tests-stack.ts
+++ integration-tests/lib/integration-tests-stack.ts
@@ -3,14 +3,33 @@ import * as route53 from "@aws-cdk/aws-route53"
 
 import { SuperEks } from "@superluminar-io/super-eks"
 
+const ROUTE53_ZONE_NAME = "integration.super-eks.superluminar.io";
+
 export class IntegrationTestsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     const hostedZone = new route53.PublicHostedZone(this, "HostedZone", {
-      zoneName: "integration.super-eks.superluminar.io",
+      zoneName: ROUTE53_ZONE_NAME,
     })
 
-    new SuperEks(this, "EksCluster", { hostedZone })
+    const superEks = new SuperEks(this, "EksCluster", { hostedZone });
+
+    // Add nginx installation for testing
+    superEks.cluster.addHelmChart("nginx", {
+      createNamespace: true,
+      namespace: "nginx",
+      repository: "https://charts.bitnami.com/bitnami",
+      chart: "nginx",
+      release: "nginx",
+      version: "8.5.2",
+      values: {
+        ingress: {
+          enabled: true,
+          hostname: `nginx.${ROUTE53_ZONE_NAME}`
+        }
+      }
+    })
+
   }
 }
