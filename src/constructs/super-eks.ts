c44719711ec584ce3eb80efc72fd046bdd7eee33
Jan Brauer <jan.brauer@superluminar.io>
2021-02-04 12:09:58 +0100

    feat: Whitelist IAM roles for admin access

--- src/constructs/super-eks.ts
+++ src/constructs/super-eks.ts
@@ -1,5 +1,6 @@
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as eks from '@aws-cdk/aws-eks';
+import * as iam from '@aws-cdk/aws-iam';
 import { IHostedZone } from '@aws-cdk/aws-route53';
 import * as cdk from '@aws-cdk/core';
 
@@ -8,13 +9,30 @@ import { AwsLoadBalancerController } from './aws-load-balancer-controller';
 import { ExternalDNS } from './external-dns';
 import { FluentBit } from './fluent-bit';
 
-
+/**
+ * Properties to configure SuperEks.
+ */
 export interface SuperEksProps {
+  /**
+   * A hosted zone for DNS management. Records in this zone will be created for your workloads by 'external-dns'.
+   */
   readonly hostedZone: IHostedZone;
+  /**
+   * A VPC, otherwise a dedicated VPC will be created.
+   * @default - none
+   */
   readonly vpc?: ec2.IVpc;
-  // adminRole?
+  /**
+   * A list of IAM roles for administrative access.
+   * The specified IAM roles will be added to the system:masters RBAC group,
+   * which means that anyone that can assume these will be able to administer this Kubernetes system.
+   */
+  readonly adminRoles?: iam.IRole[];
 }
 
+/**
+ * SuperEks wraps eks.Cluster to include batteries
+ */
 export class SuperEks extends cdk.Construct {
   private props: SuperEksProps
 
@@ -25,11 +43,21 @@ export class SuperEks extends cdk.Construct {
     this.props = props;
 
     this.cluster = this.configureCluster();
+    this.addAdminRoles();
+
     this.addSuperEksNodegroup();
 
     this.configureExternalDNS();
     this.configureAwsLoadBalancerController();
     this.configureFluentBit();
+  }
+
+  private addAdminRoles() {
+    if (this.props.adminRoles) {
+      for (const role of this.props.adminRoles) {
+        this.cluster.awsAuth.addMastersRole(role);
+      }
+    }
   }
 
   private configureCluster(): eks.Cluster {
